fca6e625e402fd48f0bf620de8f3a0b7f8abaf70
Ekrem <ekoguillot@gmail.com>
2019-03-08 17:27:23 -0500

    Updates

---
+++ precache-manifest.9e11fb75f24fd13497e115255c9df560.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d887801f950522ed9b975352df899ff",
+    "url": "/robofriends/static/media/OCRAStd.2d887801.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.dec20aab.js"
+  },
+  {
+    "revision": "63a105b8672e54139717",
+    "url": "/robofriends/static/js/main.b841d5ac.chunk.js"
+  },
+  {
+    "revision": "fd1190e5484f9b34175e",
+    "url": "/robofriends/static/js/2.791769a6.chunk.js"
+  },
+  {
+    "revision": "63a105b8672e54139717",
+    "url": "/robofriends/static/css/main.94377480.chunk.css"
+  },
+  {
+    "revision": "fd1190e5484f9b34175e",
+    "url": "/robofriends/static/css/2.1ec86210.chunk.css"
+  },
+  {
+    "revision": "768e0b6ce602a4df888b1fd8045f7da8",
+    "url": "/robofriends/index.html"
+  }
+];
